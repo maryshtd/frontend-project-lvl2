a87030e4981b8aca5eef4762c972ffea0d7fd9b4
Mariia Shtoda <maryshtd@gmail.com>
2022-02-09 10:54:00 +0100

    fixes for plain format

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -2,8 +2,8 @@ const printValue = (value) => {
     if (value === null) {
         return null;
     }
-    if (value === true || value === false) {
-        return value;
+    if (value === true || value === false || value === 0) {
+        return `${value}`;
     }
     if (typeof value === 'object') {
         return '[complex value]';
@@ -13,25 +13,25 @@ const printValue = (value) => {
 
 const plain = (tree) => {
     const format = (data, parentName) => {
-        return data.map((item) => {
+        return data
+            .filter((item) => item.type !== 'unchanged')
+            .map((item) => {
             const printKey = parentName != '' ? `${parentName}.${item.key}` : item.key;
             switch (item.type) {
                 case 'added':
-                    return `Property '${printKey}' was added with value: ${printValue(item.value)}\n`;
+                    return `Property '${printKey}' was added with value: ${printValue(item.value)}`;
                 case 'removed':
-                    return `Property '${printKey}' was removed\n`;
+                    return `Property '${printKey}' was removed`;
                 case 'updated':
-                    return `Property '${printKey}' was updated. From ${printValue(item.value1)} to ${printValue(item.value2)}\n`;
+                    return `Property '${printKey}' was updated. From ${printValue(item.value1)} to ${printValue(item.value2)}`;
                 case 'children':
-                    return format(item.children, printKey).join('');
-                case 'unchanged':
-                    return;
+                    return `${format(item.children, printKey).join('\n')}`;
                 default:
                     throw new Error(`Unknown type ${item.type}`);
             }
         })
     }
-    return format(tree, '').join('');
+    return format(tree, '').join('\n');
 };
 
 export default plain;
