925ac17a134c2fc868cbd6bda7494f9a231e6639
Mariia Shtoda <maryshtd@gmail.com>
2022-02-09 11:14:27 +0100

    linting

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,9 +1,8 @@
 import { test, expect } from '@jest/globals';
 import path, { dirname } from 'path';
-import compareFiles from '../src/index.js';
 import * as fs from 'fs';
 import { fileURLToPath } from 'url';
-
+import compareFiles from '../src/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -12,26 +11,26 @@ const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', 
 
 const cases = [
   {
-    fileName1: 'file1.json', fileName2: 'file2.json', expectedFixture: 'resultStylish.txt', format: 'stylish'
+    fileName1: 'file1.json', fileName2: 'file2.json', expectedFixture: 'resultStylish.txt', format: 'stylish',
   },
   {
-    fileName1: 'file1.yml', fileName2: 'file2.yml', expectedFixture: 'resultStylish.txt', format: 'stylish'
+    fileName1: 'file1.yml', fileName2: 'file2.yml', expectedFixture: 'resultStylish.txt', format: 'stylish',
   },
   {
-    fileName1: 'file1.json', fileName2: 'file2.json', expectedFixture: 'resultPlain.txt', format: 'plain'
+    fileName1: 'file1.json', fileName2: 'file2.json', expectedFixture: 'resultPlain.txt', format: 'plain',
   },
   {
-    fileName1: 'file1.yml', fileName2: 'file2.yml', expectedFixture: 'resultPlain.txt', format: 'plain'
+    fileName1: 'file1.yml', fileName2: 'file2.yml', expectedFixture: 'resultPlain.txt', format: 'plain',
   },
   {
-    fileName1: 'file1.json', fileName2: 'file2.json', expectedFixture: 'resultJson.txt', format: 'json'
+    fileName1: 'file1.json', fileName2: 'file2.json', expectedFixture: 'resultJson.txt', format: 'json',
   },
   {
-    fileName1: 'file1.yml', fileName2: 'file2.yml', expectedFixture: 'resultJson.txt', format: 'json'
+    fileName1: 'file1.yml', fileName2: 'file2.yml', expectedFixture: 'resultJson.txt', format: 'json',
   },
 ]
 
 test.each(cases)('Compare $fileName1 and $fileName2 in $format format', ({ fileName1, fileName2, expectedFixture, format }) => {
   const expected = fs.readFileSync(getFixturePath(expectedFixture), 'utf-8');
   expect(compareFiles(fileName1, fileName2, format)).toEqual(expected);
-})
+});
--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,17 +1,17 @@
-import plain from "./plain.js";
-import stylish from "./stylish.js"
+import plain from './plain.js';
+import stylish from './stylish.js';
 
 const printDiff = (format, data) => {
-    switch (format) {
-        case 'stylish':
-            return stylish(data);
-        case 'plain':
-            return plain(data);
-        case 'json':
-            return JSON.stringify(data);
-        default:
-            throw new Error('Format is not supported');
-    }
-}
+  switch (format) {
+    case 'stylish':
+        return stylish(data);
+    case 'plain':
+        return plain(data);
+    case 'json':
+        return JSON.stringify(data);
+    default:
+        throw new Error('Format is not supported');
+  }
+};
 
 export default printDiff;
