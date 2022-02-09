1b4ee45553da909bd25d26694f56028f521366ca
Mariia Shtoda <maryshtd@gmail.com>
2022-02-09 11:35:29 +0100

    linting

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,37 +1,35 @@
 const printValue = (value) => {
-    if (value === null) {
-        return null;
-    }
-    if (value === true || value === false || value === 0) {
-        return `${value}`;
-    }
-    if (typeof value === 'object') {
-        return '[complex value]';
-    } 
-    return `'${value}'`;
+  if (value === null) {
+    return null;
+  }
+  if (value === true || value === false || value === 0) {
+    return `${value}`;
+  }
+  if (typeof value === 'object') {
+    return '[complex value]';
+  }
+  return `'${value}'`;
 };
 
 const plain = (tree) => {
-    const format = (data, parentName) => {
-        return data
-            .filter((item) => item.type !== 'unchanged')
-            .map((item) => {
-            const printKey = parentName != '' ? `${parentName}.${item.key}` : item.key;
-            switch (item.type) {
-                case 'added':
-                    return `Property '${printKey}' was added with value: ${printValue(item.value)}`;
-                case 'removed':
-                    return `Property '${printKey}' was removed`;
-                case 'updated':
-                    return `Property '${printKey}' was updated. From ${printValue(item.value1)} to ${printValue(item.value2)}`;
-                case 'children':
-                    return `${format(item.children, printKey).join('\n')}`;
-                default:
-                    throw new Error(`Unknown type ${item.type}`);
-            }
-        })
-    }
-    return format(tree, '').join('\n');
+  const format = (data, parentName) => data
+    .filter((item) => item.type !== 'unchanged')
+    .map((item) => {
+      const printKey = parentName !== '' ? `${parentName}.${item.key}` : item.key;
+      switch (item.type) {
+        case 'added':
+          return `Property '${printKey}' was added with value: ${printValue(item.value)}`;
+        case 'removed':
+          return `Property '${printKey}' was removed`;
+        case 'updated':
+          return `Property '${printKey}' was updated. From ${printValue(item.value1)} to ${printValue(item.value2)}`;
+        case 'children':
+          return `${format(item.children, printKey).join('\n')}`;
+        default:
+          throw new Error(`Unknown type ${item.type}`);
+      };
+    });
+  return format(tree, '').join('\n');
 };
 
 export default plain;
--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -9,34 +9,32 @@ const stringify = (value, depth) => {
   }
   const valueItems = Object
     .entries(value)
-    .map(([key, value]) => `${indent(depth + 1)}  ${key}: ${stringify(value, depth + 1)}`);
+    .map(([key, val]) => `${indent(depth + 1)}  ${key}: ${stringify(val, depth + 1)}`);
   const result = ['{', ...valueItems, `${indent(depth)}  }`];
   return result.join('\n');
-}
+};
 
 const stylish = (tree) => {
-  const format = (data, depth) => {
-    return data.map((item) => {
-      const print = (value, depth, sign) => {
-      const result = `${indent(depth)}${sign} ${item.key}: ${stringify(value, depth)}\n`;
-        return result;
-      }
-      switch (item.type) {
-        case 'unchanged':
-          return print(item.value, depth, ' ');
-        case 'added':
-          return print(item.value, depth, '+');
-        case 'removed':
-          return print(item.value, depth, '-');
-        case 'updated':
-          return `${print(item.value1, depth, '-')}${print(item.value2, depth, '+')}`;
-        case 'children':
-          return `${indent(depth)}  ${item.key}: {\n${format(item.children, depth + 1).join('')}${indent(depth)}  }\n`;
-        default:
-          throw new Error(`Unknown type ${item.type}`);
-      }
-    });
-  }
+  const format = (data, depth) => data.map((item) => {
+    const print = (value, nodeDepth, sign) => {
+      const result = `${indent(nodeDepth)}${sign} ${item.key}: ${stringify(value, nodeDepth)}\n`;
+      return result;
+    };
+    switch (item.type) {
+    case 'unchanged':
+        return print(item.value, depth, ' ');
+    case 'added':
+        return print(item.value, depth, '+');
+    case 'removed':
+        return print(item.value, depth, '-');
+    case 'updated':
+        return `${print(item.value1, depth, '-')}${print(item.value2, depth, '+')}`;
+    case 'children':
+        return `${indent(depth)}  ${item.key}: {\n${format(item.children, depth + 1).join('')}${indent(depth)}  }\n`;
+    default:
+        throw new Error(`Unknown type ${item.type}`);
+    };
+  });
   return `{\n${format(tree, 1).join('')}}`;
 };
 
